229e5136b56ffa6488abd81647d5811a5371ce8a
Xilin Sun <s.sn.giraffe@gmail.com>
2016-01-24 14:58:39 -0800

    Add a script to find the relationship between numbers of prior art citations and numbers of blocking actions, and types of blocking actions

---
+++ src/citation_vs_blocking.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 14:56:23 2016
+
+@author: Xilin Sun <s.sn.giraffe AT gmail.com>
+
+Get the number of prior art citations for each Motorola patent;
+Get the number of blocking actions for each Motorola patent;
+Find the relationship between these numbers;
+
+"""
+
